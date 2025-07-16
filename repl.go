d16961f492d94aa49b80f5dbc9725ee73e1f50f3
mcbk51 <marcopoulut@gmail.com>
2025-07-16 13:58:55 -0700

    test cases

--- repl.go
+++ repl.go
@@ -1,6 +1,27 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		firstWord := words[0]
+		fmt.Printf("Your command was: %s\n", firstWord)
+	}
+}
 
 func cleanInput(text string) []string {
 	lowText := strings.ToLower(text)
