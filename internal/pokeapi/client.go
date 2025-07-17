cd0b5f5abc5d3ebff3f0965b44f9d5dc98fbd29f
mcbk51 <marcopoulut@gmail.com>
2025-07-17 11:17:55 -0700

    cache completed

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -3,16 +3,18 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+
+	"github.com/mcbk51/pokedex/internal/pokecache"
 )
 
-// Client -
 type Client struct {
+	cache      pokecache.Cache
 	httpClient http.Client
 }
 
-// NewClient -
-func NewClient(timeout time.Duration) Client {
+func NewClient(timeout, cacheInterval time.Duration) Client {
 	return Client{
+		cache: pokecache.NewCache(cacheInterval),
 		httpClient: http.Client{
 			Timeout: timeout,
 		},
